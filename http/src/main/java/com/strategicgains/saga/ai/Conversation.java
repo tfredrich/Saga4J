e96263ce71ed980d58ef9684d0249827ee9bbe6f
toddfredrich <toddfredrich@localhost>
2024-12-02 08:08:10 -0700

    Introduced Conversation model.

---
+++ http/src/main/java/com/strategicgains/saga/ai/Conversation.java
@@ -0,0 +1,125 @@
+package com.strategicgains.saga.ai;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents the entire context of a conversation with an LLM model.
+ */
+public class Conversation {
+	private String model;
+	private List<Message> messages = new ArrayList<>();
+	private Boolean stream;
+
+	public Conversation()
+	{
+		super();
+	}
+
+	public Conversation(String model)
+	{
+		this();
+		setModel(model);
+	}
+
+	public Conversation setModel(String model)
+	{
+		this.model = model;
+		return this;
+	}
+
+	public String getModel()
+	{
+		return model;
+	}
+
+	public Conversation shouldStream(boolean value)
+	{
+		this.stream = Boolean.valueOf(value);
+		return this;
+	}
+
+	public boolean isStream()
+	{
+		return stream;
+	}
+
+	public Conversation withSystem(String prompt)
+	{
+		messages.add(new SystemMessage(prompt));
+		return this;
+	}
+
+	public Conversation withUser(String prompt)
+	{
+		messages.add(new UserMessage(prompt));
+		return this;
+	}
+
+	public Conversation withAssistant(String prompt)
+	{
+		messages.add(new AssistantMessage(prompt));
+		return this;
+	}
+
+	public Message getLastMessage()
+	{
+		return messages.getLast();
+	}
+
+	public class Message
+	{
+		private String role;
+		private String content;
+
+		public Message(String role, String content)
+		{
+			super();
+			this.content = content;
+			this.role = role;
+		}
+
+		public String getRole()
+		{
+			return role;
+		}
+
+		public String getContent()
+		{
+			return content;
+		}
+	}
+
+	public class AssistantMessage
+	extends Message
+	{
+		public static final String ROLE = "assistant";
+
+		public AssistantMessage(String content)
+		{
+			super(ROLE, content);
+		}
+	}
+
+	public class SystemMessage
+	extends Message
+	{
+		public static final String ROLE = "system";
+
+		public SystemMessage(String content)
+		{
+			super(ROLE, content);
+		}
+	}
+
+	public class UserMessage
+	extends Message
+	{
+		public static final String ROLE = "user";
+
+		public UserMessage(String content)
+		{
+			super(ROLE, content);
+		}
+	}
+}
